ba7f0cdb23a7aec711ed37fa939ba8a3aa8f51f0
hanwil123 <farhanwildan12366@gmail.com>
2022-12-16 10:52:08 +0700

    udah 80% layout nya

--- src/components/Tugas.jsx
+++ src/components/Tugas.jsx
@@ -13,25 +13,28 @@ const headerMatkul = [
     headermatkul: 'Algoritma Pemrograman',
     jadwal1: '9 Desember 2022',
     tanggal2: '20 Desember 2022'
+  },
+  {
+    id: 3,
+    headermatkul: 'Bahasa Inggris',
+    jadwal1: '20 Desember 2022',
+    tanggal2: '5 Januari 2023'
   }
 ];
 
 const Tugas = () => {
   return (
     <section className="">
-      <div className="contianer mx-auto flex justify-evenly w-full h-full border rounded-xl">
-        <div className=" flex flex-col bg-yellow-500 space-y-20">
+      <div className="contianer mx-auto flex justify-evenly w-full h-full rounded-xl pt-8">
+        <div className=" flex flex-col space-y-16">
           <h1 className=" text-xl text-center">List Tugas Matkul</h1>
           <div>
-            <table className=" w-[350px] h-[500px]">
+            <table className=" w-[350px] h-[500px] outline outline-offset-2 outline-blue-900 ring-offset-2 ring-4 rounded-2xl ">
               {headerMatkul.map((header) => {
                 return (
                   <tr className="" key={header.id}>
-                    <td className="  border rounded-3xl m-5 box-content p-5 bg-black space-y-7">
-                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex">
-                        <h1>{header.headermatkul}</h1>
-                      </td>
-                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex">
+                    <td className="  border rounded-3xl m-5 box-content p-5 bg-[#D0B8A8] space-y-7">
+                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex justify-center">
                         <h1>{header.headermatkul}</h1>
                       </td>
                     </td>
@@ -46,15 +49,15 @@ const Tugas = () => {
             <BsArrowRight />
           </h1>
         </div>
-        <div className=" flex flex-col bg-slate-500 space-y-20">
+        <div className=" flex flex-col space-y-16">
           <h1 className=" text-xl text-center">Jadwal Diberi Tugas</h1>
           <div>
-            <table className=" w-[350px] h-[500px]">
+            <table className=" w-[350px] h-[500px] outline outline-offset-2 outline-pink-500 ring-offset-2 ring-4 rounded-2xl">
               {headerMatkul.map((header) => {
                 return (
                   <tr className="" key={header.id}>
-                    <td className="  border rounded-3xl m-5 box-content p-5 bg-black space-y-7">
-                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex">
+                    <td className="  border rounded-3xl m-5 box-content p-5 bg-[#D0B8A8] space-y-7">
+                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-800 flex justify-center">
                         <h1>{header.jadwal1}</h1>
                       </td>
                     </td>
@@ -69,16 +72,16 @@ const Tugas = () => {
             <BsArrowRight />
           </h1>
         </div>
-        <div className=" flex flex-col bg-slate-500 space-y-20">
+        <div className=" flex flex-col space-y-16">
           <h1 className=" text-xl text-center">Jadwal Deadline Tugas</h1>
           <div>
-            <table className=" w-[350px] h-[500px]">
+            <table className=" w-[350px] h-[500px] outline outline-offset-2 outline-pink-500 ring-offset-2 ring-4 rounded-2xl">
               {headerMatkul.map((header) => {
                 return (
                   <tr className="" key={header.id}>
-                    <td className="  border rounded-3xl m-5 box-content p-5 bg-black space-y-7">
-                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex">
-                        <h1>{header.tanggal2}</h1>
+                    <td className="  border rounded-3xl m-5 box-content p-5 bg-[#D0B8A8] space-y-7">
+                      <td className="  border rounded-3xl m-4 box-content p-4 bg-red-700 flex justify-center">
+                        <h1 className="">{header.tanggal2}</h1>
                       </td>
                     </td>
                   </tr>
