95aa44b570b106beed42ec237ff393ea859b1192
hanwil123 <farhanwildan12366@gmail.com>
2022-12-16 10:51:43 +0700

    nambahin link di matkul nya

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -80,24 +80,28 @@ const jadwalKuliah = [
     matkul: [
       {
         namaMatkul: 'Algoritma & Pemrograman 1A',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=52824',
         jam: '07.30-9.30',
         ruang: 'G345',
         dosen: dosen[0]
       },
       {
         namaMatkul: 'Algoritma & Pemrograman 1B',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=45933',
         jam: '09.30-11.30',
         ruang: 'G345',
         dosen: dosen[1]
       },
       {
         namaMatkul: 'Pendidikan Kewarganegaraan',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=48968',
         jam: '13.30-15.30',
         ruang: 'G313',
         dosen: dosen[2]
       },
       {
         namaMatkul: 'Matematika Dasar 1',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=49135',
         jam: '15.30-17.30',
         ruang: 'G313',
         dosen: dosen[3]
@@ -113,18 +117,21 @@ const jadwalKuliah = [
     matkul: [
       {
         namaMatkul: 'Ilmu Budaya Dasar',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=50592',
         jam: '09.30-11.30',
         ruang: 'E231',
         dosen: dosen[4]
       },
       {
         namaMatkul: 'Matematika Informatika 1 **',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=46874',
         jam: '11.30-13.30',
         ruang: 'E231',
         dosen: dosen[5]
       },
       {
         namaMatkul: 'Algoritma & Pemrograman 1C',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=51237',
         jam: '13.30-15.30',
         ruang: 'E231',
         dosen: dosen[6]
@@ -136,18 +143,21 @@ const jadwalKuliah = [
     matkul: [
       {
         namaMatkul: 'Peng. Tekno. Komp. & Inf. A **',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=51228',
         jam: '07.30-09.30',
         ruang: 'G223',
         dosen: dosen[7]
       },
       {
         namaMatkul: 'Bahasa Inggris',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=48390',
         jam: '09.30-11.30',
         ruang: 'G223',
         dosen: dosen[8]
       },
       {
         namaMatkul: 'Peng. Tekno. Komp. & Inf. C **',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=48321',
         jam: '12.30-14.30',
         ruang: 'G344',
         dosen: dosen[9]
@@ -159,18 +169,21 @@ const jadwalKuliah = [
     matkul: [
       {
         namaMatkul: 'Pendidikan Agama Islam	',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=51552',
         jam: '07.30-9.30',
         ruang: 'E427',
         dosen: dosen[10]
       },
       {
         namaMatkul: 'Peng. Tekno. Komp. & Inf. B **	',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=52670',
         jam: '09.30-11.30',
         ruang: 'E427',
         dosen: dosen[11]
       },
       {
         namaMatkul: 'Fisika & Kimia Dasar 1A',
+        link: 'https://v-class.gunadarma.ac.id/course/view.php?id=48300',
         jam: '13.30-15.30',
         ruang: 'E427',
         dosen: dosen[12]
@@ -192,30 +205,36 @@ export const Menu = () => {
         <section>
           <div className="container-xl">
             <div className="flex flex-col justify-center items-center mx-auto w-full h-screen ">
-              <h1 className="text-4xl font-bold xl:text-7xl md:text-5xl">Selamat datang Di Website</h1>
+              <h1 className="text-4xl font-bold xl:text-7xl md:text-5xl">
+                Selamat datang Di Website
+              </h1>
               <h3 className="text-4xl mt-4 xl:text-5xl font-semibold">1IA10</h3>
             </div>
           </div>
         </section>
 
         <section id="jadwal" className="pt-10">
-					<h1 className="text-4xl font-bold  text-center mb-20 ">Jadwal Pelajaran</h1>
+          <h1 className="text-4xl font-bold  text-center mb-20 ">Jadwal Pelajaran</h1>
           <div className="container mx-auto flex flex-wrap justify-center text-center">
             {jadwalKuliah.map((jadwal, index) => {
               if (jadwal.matkul.length === 0) return <div key={index}></div>;
               return (
                 <div
-                  className="w-[320px] h-[630px]  border shadow-lg shadow-black bg-blue-100 text-black rounded-2xl m-5 box-content px-5 py-3"
+                  className="w-[320px] h-[730px]  border shadow-lg shadow-black bg-blue-100 text-black rounded-2xl m-5 box-content px-5 py-3"
                   key={index}>
                   <div className="text-xl font-bold mb-3">{jadwal.hari.toUpperCase()}</div>
                   {jadwal.matkul.map((matkul, index) => {
                     return (
-                      <div key={index} className="mb-4 box-content bg-white border border-black  rounded-xl p-3">
+                      <div
+                        key={index}
+                        className="mb-4 box-content bg-white border border-black  rounded-xl p-3">
                         <table className="flex flex-col items-start ">
                           <tr className="">
                             <td>Matkul</td>
                             <td>:</td>
-                            <td>{matkul.namaMatkul}</td>
+                            <a href={matkul.link}>
+                              <td>{matkul.namaMatkul}</td>
+                            </a>
                           </tr>
                           <tr>
                             <td>Jam</td>
